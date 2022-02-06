b6c422fd534d7791ddc85e300528fd72e445049a
11jem <cabalidajem@gmail.com>
2022-02-05 23:42:19 -0600

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,12 @@ app.set('views', path.join(__dirname, 'views'));
 //////////////////////////////////
 // Global Middlewares
 //////////////////////////////////
+
+// Implement CORS
+app.use(cors());
+
+// Responding to options request when there is a preflight phase
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
